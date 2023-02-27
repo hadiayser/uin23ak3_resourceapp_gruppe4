530fb3ebf9615ff9b8e8c5662b263619bd815d26
marielve <mariel99@live.no>
2023-02-27 19:01:56 +0100

    Lagt inn React Router

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Main from './Components/Main';
 import ArticleCard from './Components/ArticleCard';
 import H1 from './Components/H1';
 import Nav from './Components/Nav';
+import { Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="App">
-      <Main/>
-    </div>
+    <Routes>
+      <Route index element={<Main />} /> 
+      <Route path="main" element={<Main />} />
+    </Routes>
   );
 }
 
