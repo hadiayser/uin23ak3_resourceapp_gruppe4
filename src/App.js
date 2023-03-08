df833a811b43fc29bc76de8f98f0937d247ffed3
hadiayser <111729440+hadiayser@users.noreply.github.com>
2023-03-08 20:08:28 +0100

    Ryddet opp i koden og sletter articlecard.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
-import logo from './logo.svg';
 import './css/main.css';
 import Main from './Components/Main';
-import ArticleCard from './Components/ArticleCard';
-import H1 from './Components/H1';
-import Nav from './Components/Nav';
 import { Route, Routes } from 'react-router-dom';
-
 
 function App() {
   return (
