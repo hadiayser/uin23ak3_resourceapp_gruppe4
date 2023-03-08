2c52d04275674b04d45ad539291f3ba0fa00ac89
marielve <mariel99@live.no>
2023-03-08 12:23:01 +0100

    La inn routes i app.js

--- src/App.js
+++ src/App.js
@@ -10,8 +10,11 @@ import { Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <Routes>
-      <Route index element={<Main />} /> 
-      <Route path="main" element={<Main />} />
+      <Route path="/" element={<Main category="html" />} />
+      <Route path="html" element={<Main category="html" />} />
+      <Route path="/css" element={<Main category="css" />} />
+      <Route path="/react" element={<Main category="react" />} />
+      <Route path="/headless-cms" element={<Main category="headles-cms" />} />
     </Routes>
   );
 }
